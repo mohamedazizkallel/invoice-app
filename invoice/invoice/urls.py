b637e5a052c71de036a6e993615790dc22db2be1
AzizKallel2019 <162117059+AzizKallel2019@users.noreply.github.com>
2026-02-19 19:56:16 +0100

    modified:   invoice/invoice/__pycache__/urls.cpython-313.pyc 	modified:   invoice/invoice/urls.py

--- invoice/invoice/urls.py
+++ invoice/invoice/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +10,8 @@ urlpatterns = [
 ]
 
 
-# Serve media files in development
+# Serve media files and debug toolbar in development only
 if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += debug_toolbar_urls()
