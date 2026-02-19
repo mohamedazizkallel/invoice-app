c05a0a1f36e41db1a9564d7936c117e1410c8fca
AzizKallel2019 <162117059+AzizKallel2019@users.noreply.github.com>
2026-02-19 20:02:13 +0100

    modified:   invoice/sales/__pycache__/admin.cpython-313.pyc 	modified:   invoice/sales/admin.py

--- invoice/sales/admin.py
+++ invoice/sales/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Client,Settings,Invoice,ClientTransaction
+from .models import Client, InvoiceService, InvoiceSupplyUsage, Purchase, PurchaseLine,Settings,Invoice,ClientTransaction
 
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Settings)
 admin.site.register(Invoice)
 admin.site.register(ClientTransaction)
+admin.site.register(Purchase)
+admin.site.register(PurchaseLine)
+admin.site.register(InvoiceSupplyUsage)
+admin.site.register(InvoiceService)
