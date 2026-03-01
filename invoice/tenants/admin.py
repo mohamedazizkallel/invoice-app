4a7f5ea08df23043f831e45b656c094563863572
AzizKallel2019 <162117059+AzizKallel2019@users.noreply.github.com>
2026-03-01 22:51:45 +0100

    modified:   invoice/tenants/admin.py

--- invoice/tenants/admin.py
+++ invoice/tenants/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
+from django.db import connection
+from django_tenants.utils import get_public_schema_name
+
 from tenants.models import Tenant, Domain, TenantUser
 
 
 @admin.register(Tenant)
 class TenantAdmin(admin.ModelAdmin):
     list_display = ('name', 'schema_name', 'is_active', 'created_on')
+
+    def save_model(self, request, obj, form, change):
+        # Tenant creation/update must happen on the public schema.
+        # The middleware has already set the schema to the admin's own tenant,
+        # so we switch back to public before saving.
+        connection.set_schema_to_public()
+        super().save_model(request, obj, form, change)
+
+    def delete_model(self, request, obj):
+        connection.set_schema_to_public()
+        super().delete_model(request, obj)
 
 
 @admin.register(Domain)
